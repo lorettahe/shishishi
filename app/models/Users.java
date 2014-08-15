22eb726449a79551139f3943060037856e15c571
Loretta <loretta@swiftkey.net>
2014-08-15 14:55:27 +0100

    Adding users id sequence generation config

--- app/models/Users.java
+++ app/models/Users.java
@@ -4,6 +4,8 @@ import play.data.validation.Constraints;
 import play.db.ebean.Model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 /**
@@ -12,6 +14,7 @@ import javax.persistence.Id;
 @Entity
 public class Users extends Model {
     @Id
+    @GeneratedValue(strategy= GenerationType.SEQUENCE, generator="users_id_seq")
     public Long id;
 
     @Constraints.Required
